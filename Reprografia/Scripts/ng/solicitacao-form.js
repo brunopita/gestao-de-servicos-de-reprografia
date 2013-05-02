427be3d1d4c598ad36213f0ddece929ea82c7575
paolobueno <paolohaji@gmail.com>
2013-05-02 16:57:15 -0300

    Avanco no form angular

--- Reprografia/Scripts/ng/solicitacao-form.js
+++ Reprografia/Scripts/ng/solicitacao-form.js
@@ -1,24 +1,28 @@
 angular.module('solicitacao-form', []);
 
-var SolicitacaoFormCtrl = function ($scope, seed) {
+var SolicitacaoFormCtrl = function ($scope) {
   // Inicializar scope
-  $scope = {};
-  if (seed != null) {$scope = seed};
-  
+  $scope.Itens = [];
+
   $scope.Formatos = ["A4", "A3", "A5"];
-  $scompe.Solicitacao.Formato = "A4";
-
   $scope.Suportes = ["E-Mail", "Zipdrive", "Papel", "CD"];
-  $scope.Solicitacao.Suporte = "Papel"
 
-    $scope.Areas = window.Areas;
-    $scope.Fornecedores = window.Fornecedores;
-    $scope.Codificacoes = window.Codificacoes;
+  //Padrões
+  $scope.Solicitacao = {};
+  $scope.Solicitacao.Formato = "A4";
+  $scope.Solicitacao.Suporte = "Papel";
+  $scope.Solicitacao.Area = "1";
+  $scope.Solicitacao.Fornecedor = "1";
+
+  //Pegar informações de seeding
+  $scope.Areas = window.Areas;
+  $scope.Fornecedores = window.Fornecedores;
+  $scope.Codificacoes = window.Codificacoes;
 
   // Valor padrão: 11541 - 404503 : Educacao e Tecnologia - Especializacao Prof. Nivel Basico - Entidade
-  $scope.Solicitacao.Codificacao = 23; 
+  $scope.Solicitacao.Codificacao = 23;
 
   $scope.addNew = function () {
     $scope.Itens.push({});
-  }
-}
+  };
+};
